d9050f6c95ef2469b9a13a55734e7d507511e632
Gabs-jg <gabriel19joao@gmail.com>
2025-03-25 01:58:54 -0300

    Aritmetica em java

---
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/src/Aritmetica.java
@@ -0,0 +1,20 @@
+public class Aritmetica {
+    public static void main(String[] args) {
+        int a = 5, b = 2; //Declara e inicia variáveis
+        //Operações aritméticas
+        System.out.println("Valores: a = " + a + ", b = " + b);
+        System.out.println(" -b = " + (-b)); //Valor de b negativo
+        System.out.println(" -a = " + (-a)); //Valor de a negativo
+        System.out.println("a + b = " + (a + b)); //Soma
+        System.out.println("a - b = " + (a - b)); //Subtração
+        System.out.println("a * b = " + (a *b)); //Multiplicação
+        System.out.println("a / b = " + (a / b)); //Divisão inteira
+        System.out.println("(float) a / b = " + ((float) a / b )); //Divisão exata
+        System.out.println("a % b = " + (a % b)); //Resto da divisão
+        System.out.println("a++ = " + (a++)); //Incremento em a
+        System.out.println("b++ = " + (b++)); //Incremento em b
+        System.out.println(" b = " + (b)); //Valor de b
+        System.out.println(" a = " + (a)); //Valor de a
+        System.out.println("Valores: a = " + a + " b = " + b); //Valor de a e b
+    }
+}
