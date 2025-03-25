b6a40de495d7eb902dd8d93e2707ef4e047cd50a
Gabs-jg <gabriel19joao@gmail.com>
2025-03-25 02:15:56 -0300

    Exemplo For

---
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        int j;
+        //Efetua a contagem de 0 a 0
+        for(j=0; j<10; j++) {
+            System.out.println(j);
+        }
+    }
+}
