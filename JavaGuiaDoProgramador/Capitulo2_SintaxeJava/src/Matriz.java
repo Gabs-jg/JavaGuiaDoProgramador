4a95134dd2000ee75bc12ddb431acba3e888129d
Gabs-jg <gabriel19joao@gmail.com>
2025-03-27 04:15:21 -0300

    Matriz Triangular

--- JavaGuiaDoProgramador/Capitulo2_SintaxeJava/src/Matriz.java
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/src/Matriz.java
@@ -1,13 +1,13 @@
 public class Matriz {
     public static void main(String[] args) {
         double m[][] = new double [3][4]; //Declara matriz 3x4
-        for(int l=0; l<m.length; l++) {
-            for(int c=0; c<m.length; c++) {
+        for(int l=0; l<m.length; l++) { //Preenche matriz
+            for(int c=0; c<m[l].length; c++) {
                 m[l][c] = l*m[l].length + c;
             }
         }
-        for(int l=0; l<m.length; l++) {
-            for(int c=0; c<m.length; c++) {
+        for(int l=0; l<m.length; l++) { //Exibe matriz
+            for(int c=0; c<m[l].length; c++) {
                 System.out.print(m[l][c] + "\t");
             }
             System.out.println();
---
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/src/MatrizTriang.java
@@ -0,0 +1,19 @@
+public class MatrizTriang {
+    public static void main(String[] args) {
+        double m[][]; //Declara matriz
+        m = new double [4][]; //Aloca matriz parcialmente
+        int aux = 0; //Variável auxiliar
+        for(int l=0; l<m.length; l++) { 
+            m[l] = new double[l+1]; //Completa a alocação
+            for(int c=0; c<m[l].length; c++) { //Preenche a matriz
+                m[l][c] = aux++;
+            }
+        }
+        for(int l=0; l<m.length; l++) { //Exibe matriz
+            for(int c=0; c<m[l].length; c++) {
+                System.out.print(m[l][c] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
