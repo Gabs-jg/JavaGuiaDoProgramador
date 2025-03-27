6e3b19c63ef77dbc0edb397e1448b6a9ac9ab515
Gabs-jg <gabriel19joao@gmail.com>
2025-03-27 13:30:58 -0300

    Exercício 8

---
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/ExerciciosDeRevisao/src/MaiorNumero.java
@@ -0,0 +1,26 @@
+package JavaGuiaDoProgramador.Capitulo2_SintaxeJava.ExerciciosDeRevisao.src;
+import java.util.Scanner;
+
+public class MaiorNumero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o primeiro número: ");
+        double n1 = sc.nextDouble();
+        System.out.print("Digite o segundo número: ");
+        double n2 = sc.nextDouble();
+        System.out.print("Digite o terceiro número: ");
+        double n3 = sc.nextDouble();
+
+        if(n1>n2) {
+            if(n1>n3) {
+                System.out.println("O número " + n1 + " é o maior número entre os números informados.");
+            } else {
+                System.out.println("O número " + n3 + " é o maior número entre os números informados.");
+            }
+        } else if(n2>n3) {
+            System.out.println("O número " + n2 + " é o maior número entre os números informados.");
+        } else {
+            System.out.println("O número " + n3 + " é o maior número entre os números informados.");
+        }
+    }
+}
--- JavaGuiaDoProgramador/Capitulo2_SintaxeJava/ExerciciosDeRevisao/src/TempoMontante.java
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/ExerciciosDeRevisao/src/TempoMontante.java
@@ -1,5 +1,6 @@
 package JavaGuiaDoProgramador.Capitulo2_SintaxeJava.ExerciciosDeRevisao.src;
 import java.util.Scanner;
+//Programa que calcula o tempo necessário para alcançar um montante com um valor fixo de depósitos mensais.
 public class TempoMontante {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,7 +8,11 @@ public class TempoMontante {
         double deposito = sc.nextDouble();
         System.out.print("Informe o valor do montante final: R$");
         double montante = sc.nextDouble();
-        System.out.println("A quantidade de meses para alcançar um montante de R$" + montante + " com depósitos mensais de R$" + deposito + " é de " + calculoMeses(deposito, montante) + " meses.");
+        if(deposito <=0 || montante <=0) {
+            System.out.println("Valor de depósito ou montante inválidos.");
+        } else {
+            System.out.println("A quantidade de meses para alcançar um montante de R$" + montante + " com depósitos mensais de R$" + deposito + " é de " + calculoMeses(deposito, montante) + " meses.");
+        }
         sc.close();
     }
 
