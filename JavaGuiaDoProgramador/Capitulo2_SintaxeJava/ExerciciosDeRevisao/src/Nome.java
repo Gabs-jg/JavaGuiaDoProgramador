2175b58651bc65cd9675fd7bd485b1c67cd4fb93
Gabs-jg <gabriel19joao@gmail.com>
2025-03-27 04:23:46 -0300

    Resolução Exercício 1

---
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/ExerciciosDeRevisao/src/Nome.java
@@ -0,0 +1,15 @@
+package JavaGuiaDoProgramador.Capitulo2_SintaxeJava.ExerciciosDeRevisao;
+import java.util.Scanner;
+
+//Programa para exibir nome informado na tela.
+public class Nome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Informe um nome: ");
+        String nome = sc.nextLine();
+        
+
+        System.out.println("Nome informado: " + nome);
+    }
+    
+}
