a03df4749fd6725ff1a669e1f4acc2c6b5c810c5
Gabs-jg <gabriel19joao@gmail.com>
2025-03-28 23:08:03 -0300

    Exercício 14

--- JavaGuiaDoProgramador/Capitulo2_SintaxeJava/ExerciciosDeRevisao/src/ValoresCrescentes.java
+++ JavaGuiaDoProgramador/Capitulo2_SintaxeJava/ExerciciosDeRevisao/src/ValoresCrescentes.java
@@ -4,14 +4,55 @@ public class ValoresCrescentes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Quantos valores serão lidos? ");
-        int valores = sc.nextInt();
-        int valorInformado;
-        int numeros[] = new int [valores];
-        for(int i=0; i<valores; i++) {
-            System.out.print("Digite o valor " + (i+1) + ": ");
-            valorInformado = sc.nextInt();
-            numeros[i] = valorInformado;
+        int valores[] = new int[sc.nextInt()];
+    
+        for(int i=0; i<valores.length; i++) {
+            System.out.print("\nDigite o valor " + (i+1) + ": ");
+            valores[i] = sc.nextInt();
         }
-        System.out.println(numeros);
+
+        System.out.print("\nValores na ordem como foram fornecidos: [");
+        for(int i=0; i<valores.length; i++) {
+            if(i==valores.length-1) {
+                System.out.print(valores[i]);
+            } else {
+                System.out.print(valores[i] + ", ");
+            }
+        }
+        System.out.println("]");
+
+        boolean ordemCrescente = true;
+        for(int i=1; i<valores.length; i++) {
+            if(valores[i] < valores[i-1]) {
+                ordemCrescente = false;
+                break;
+            }
+        }
+
+        for(int i=0; i<valores.length-1; i++) {
+            for(int j=0; j<valores.length-1; j++) {
+                if(valores[j] > valores[j+1]) {
+                    int temp = valores[j];
+                    valores[j] = valores[j+1];
+                    valores[j+1] = temp;
+                }
+            } 
+        }
+        System.out.print("\nValores em ordem crescente: [");
+        for(int i=0; i<valores.length; i++) {
+            if(i==valores.length-1) {
+                System.out.print(valores[i]);
+            } else {
+                System.out.print(valores[i] + ", ");
+            }
+        }
+        System.out.println("]");
+
+        if(ordemCrescente) {
+            System.out.println("\nA sequência digitada pelo usuário estava em ordem crescente.\n");
+        } else {
+            System.out.println("\nA sequência digitada pelo usuário não estava em ordem cresecente.\n");
+        }
+        sc.close();
     }
 }
